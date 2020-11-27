bc6d41319ef25cad04502894c8d7b376aabb7d02
caio benatti <caio.benatti@gmail.com>
2020-11-27 09:49:04 +0000

    changes

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,7 +1,7 @@
-jQuery(document).ready(function () {
-  $(".button").on("click", function () {
-    $(".vernice").attr("class", "vernice verniceOver");
-    $(".brush").attr("class", "brush brushOver");
-    $(".gocce").attr("class", "gocce gocceOver");
-  });
-});
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text", { x: "0%", duration: 0.75, stagger: 0.15 });
+tl.to(".slider", { x: "-100%", duration: 0.75, delay: 0.25 });
+tl.to(".intro", { x: "-100%", duration: 0.5 }, "-=0.50");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
