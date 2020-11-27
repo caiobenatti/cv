0b2c0b075846f2ba9936f98e981637a26048b1f9
caio benatti <caio.benatti@gmail.com>
2020-11-27 13:23:29 +0000

    changes

--- libs/js/script.js
+++ libs/js/script.js
@@ -5,3 +5,4 @@ tl.to(".slider", { x: "-100%", duration: 0.75, delay: 0.25 });
 tl.to(".intro", { x: "-100%", duration: 0.5 }, "-=0.50");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".card", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
