0181c2a7bbafa13057ced55abd67c53eccb58d30
caio benatti <caio.benatti@gmail.com>
2020-11-28 12:00:36 +0000

    final

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,8 +1,46 @@
-const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+// const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".text", { x: "0%", duration: 0.75, stagger: 0.15 });
-tl.to(".slider", { x: "-100%", duration: 0.75, delay: 0.25 });
-tl.to(".intro", { x: "-100%", duration: 0.5 }, "-=0.50");
+// tl.to(".text", { x: "0%", duration: 0.75, stagger: 0.15 });
+// tl.to(".slider", { x: "-100%", duration: 0.75, delay: 0.25 });
+// tl.to(".intro", { x: "-100%", duration: 0.5 }, "-=0.50");
+// tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+// tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+// tl.fromTo(".card", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+let tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to("ul.transition li", {
+  duration: 0.5,
+  scaleY: 1,
+  transformOrigin: "bottom left",
+  stagger: 0.2,
+});
+tl.to("ul.transition li", {
+  duration: 0.5,
+  scaleY: 0,
+  transformOrigin: "bottom left",
+  stagger: 0.1,
+  delay: 0.1,
+});
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 tl.fromTo(".card", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".form", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // these IDs from the previous steps
+    emailjs.sendForm("service_km1jgrx", "template_xdhf66c", this).then(
+      function () {
+        console.log("SUCCESS!");
+        $("#buttonSend").html("Sent!");
+      },
+      function (error) {
+        console.log("FAILED...", error);
+      }
+    );
+  });
